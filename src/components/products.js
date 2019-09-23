a608b5435bb6a12902f239267e1e2c76314bf959
Googolaire <thehuntermaxwell@gmail.com>
2019-09-23 15:37:28 -0500

    refactored class to func components and cleaned up 'Problems'

--- src/components/products.js
+++ src/components/products.js
@@ -1,85 +1,84 @@
-import "bootstrap/dist/js/bootstrap.bundle.min";
-import React, { Component } from "react";
+import 'bootstrap/dist/js/bootstrap.bundle.min'
+import React from 'react'
 //Components
 // Styles
 
-import "../styles.css";
-import "bootstrap/dist/css/bootstrap.min.css";
-//let stock = "https://my-json-server.typicode.com/googolaire/instaestore";
-export default class Products extends Component {
-  render() {
-    let bikes = [];
-    if (window.location.host === "localhost:3000") {
+import '../styles.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+//let stock = 'https://my-json-server.typicode.com/googolaire/instaestore'
+const Products = () => {
+    let bikes = []
+    if (window.location.host === 'localhost:3000') {
       bikes = [
         {
           id: 1,
-          name: "Adult Male Bike",
+          name: 'Adult Male Bike',
           price: 20.5,
-          image: "http://via.placeholder.com/250x250?text=Adult%20Male%20Bike",
-          product_type: "bike"
+          image: 'http://via.placeholder.com/250x250?text=Adult%20Male%20Bike',
+          product_type: 'bike'
         },
         {
           id: 2,
-          name: "Adult Female Bike",
+          name: 'Adult Female Bike',
           price: 20.5,
           image:
-            "http://via.placeholder.com/250x250?text=Adult%20Female%20Bike",
-          product_type: "bike"
+            'http://via.placeholder.com/250x250?text=Adult%20Female%20Bike',
+          product_type: 'bike'
         },
         {
           id: 3,
-          name: "Kids Unisex Bike",
+          name: 'Kids Unisex Bike',
           price: 12.75,
-          image: "http://via.placeholder.com/250x250?text=Kids%20Unisex%20Bike",
-          product_type: "bike"
+          image: 'http://via.placeholder.com/250x250?text=Kids%20Unisex%20Bike',
+          product_type: 'bike'
         },
         {
           id: 4,
-          name: "Adult Unisex Helmet",
+          name: 'Adult Unisex Helmet',
           price: 4.0,
           image:
-            "http://via.placeholder.com/250x250?text=Adult%20Unisex%20Helmet",
-          product_type: "accessory"
+            'http://via.placeholder.com/250x250?text=Adult%20Unisex%20Helmet',
+          product_type: 'accessory'
         },
         {
           id: 5,
-          name: "Kids Unisex Helmet",
+          name: 'Kids Unisex Helmet',
           price: 3.5,
           image:
-            "http://via.placeholder.com/250x250?text=Kids%20Unisex%20Helmet",
-          product_type: "accessory"
+            'http://via.placeholder.com/250x250?text=Kids%20Unisex%20Helmet',
+          product_type: 'accessory'
         },
         {
           id: 6,
-          name: "Insurance",
+          name: 'Insurance',
           price: 9.99,
-          image: "http://via.placeholder.com/250x250?text=Insurance",
-          product_type: "addon"
+          image: 'http://via.placeholder.com/250x250?text=Insurance',
+          product_type: 'addon'
         }
-      ];
+      ]
     } else {
       return (
         <div>
-          Tell The store owner you want to order online and pickup in store{" "}
+          Tell The store owner you want to order online and pickup in store{' '}
         </div>
-      );
+      )
     }
     return (
-      <div className="App">
-        <div className="contrainer">
-          <div className="row">
+      <div className='App'>
+        <div className='contrainer'>
+          <div className='row'>
             {bikes.map(bike => (
-              <div className="col-md-4">
-                <div className="card m-2">
+              <div className='col-md-4'>
+                <div className='card m-2'>
                   <img
-                    className="card-img-top"
+                    className='card-img-top'
                     src={bike.image}
-                    alt="aaaaaaaa"
+                    alt='aaaaaaaa'
                   />
-                  <div classme="card-body">
-                    <h4 className="card-title">{bike.name}</h4>
-                    <p className="card-text">words</p>
-                    <button className="btn btn-outline-primary">Buy Now</button>
+                  <div classme='card-body'>
+                    <h4 className='card-title'>{bike.name}</h4>
+                    <p className='card-text'>words</p>
+                    <button className='btn btn-outline-primary'>Buy Now</button>
                   </div>
                 </div>
               </div>
@@ -87,6 +86,6 @@ export default class Products extends Component {
           </div>
         </div>
       </div>
-    );
+    )
   }
-}
+export default Products
